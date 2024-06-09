0785001c85ec5cb50223d1dbc8fbaa7d86192350
Slava <steschenko.vyacheslav@gmail.com>
2024-06-09 17:30:14 +0200

    Working with styling messages

--- react/src/components/Nav.jsx
+++ react/src/components/Nav.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import DirectMessage from './DirectMessage';
 
 const Nav = () => {
+    const email = localStorage.getItem('email');
     return <div className="Nav">
                 <div className="SlideMenu">
                     <DirectMessage />
                 </div>
+                <div className={"email"}>Logged in as: {email}</div>
             </div>
 };
 
